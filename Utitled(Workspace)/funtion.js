74782e13a31a34c33ea51e308d5de0f4bdfcac7d
sojibboumik <info.alexsojib@gmail.com>
2022-12-22 11:32:59 +0600

    Sojib Boumik

--- Utitled(Workspace)/funtion.js
+++ Utitled(Workspace)/funtion.js
@@ -1,27 +1,3 @@
-// // // without funtion
-
-//     var num = 16;
-// document.write("<br/> Result is:" + num * num)
-//     var num = 25;
-// document.write("<br/> Result is:" + num * num)
-//     var num = 36;
-// document.write("<br/> Result is:" + num * num)
-
-
-// // //using funtion
-// function square(){
-//     var num = 16;
-//     document.write("<br/> Result is:" + num * num)
-// }
-
-// square();
-
-// // // parameterized funtion calling
-// function square_p(num){
-// document.write("<br/>parameterized funtion result is: +num * num")
-// }
-// square_p(10);
-
 // let num1= Number(prompt("Enter first Number"));
 // let num2= Number(prompt("Enter Secound Number"));
 
@@ -35,16 +11,40 @@
 
 // math();
 
-let num1 = (Number(prompt("Enter the first number: ")));
-let num2 = (Number(prompt("Enter the secound number: ")));
-let num3 = (Number(prompt("Enter the third number: ")));
-let num4 = (Number(prompt("Enter the four number: ")));
+// let num1 = (Number(prompt("Enter the first number: ")));
+// let num2 = (Number(prompt("Enter the secound number: ")));
+// let num3 = (Number(prompt("Enter the third number: ")));
+// let num4 = (Number(prompt("Enter the four number: ")));
 
-function result(){
-    document.write('Addition is: '+(num1+num2+num3+num4)+'</br>');
-    document.write('Substraction is: '+(num1-num2-num3-num4)+'</br>');
-    document.write('Multiplication is: '+(num1*num2*num3*num4)+'</br>');
-    document.write('Divison is: '+(num1/num2/num3/num4)+'</br>');
+// function result(){
+//     document.write('Addition is: '+(num1+num2+num3+num4)+'</br>');
+//     document.write('Substraction is: '+(num1-num2-num3-num4)+'</br>');
+//     document.write('Multiplication is: '+(num1*num2*num3*num4)+'</br>');
+//     document.write('Divison is: '+(num1/num2/num3/num4)+'</br>');
+// }
+
+// result();
+
+// // without funtion
+
+var num = 16;
+document.write("<br/> Result is:" + num * num)
+    var num = 25;
+document.write("<br/> Result is:" + num * num)
+    var num = 36;
+document.write("<br/> Result is:" + num * num)
+
+
+// //using funtion
+function square(){
+    var num = 16;
+    document.write("<br/> Result is:" + num * num)
 }
 
-result();
+square();
+
+// // parameterized funtion calling
+function square_p(num){
+document.write("<br/>parameterized funtion result is: +num * num")
+}
+square_p(10);
